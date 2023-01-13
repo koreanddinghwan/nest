50f362df9dadfcf3170c67d11c25a4ecac07f479
myukang <myunghwan0421@gmail.com>
2023-01-14 08:25:59 +0900

    FEAT: ServeStaticModule을 통해 react가 사용하는 파일들이 찾을 경로를 명시한다. 만약 /api/로 시작하는 경로일경우, api 요청으로 간주하고 controller에서 처리하게된다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CatsModule } from './cats/cats.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*'],
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  // constructor(private DataSource: DataSource) {}
+}
