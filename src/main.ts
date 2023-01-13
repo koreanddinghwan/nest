6d51e8c1facf0ceb50eb82894639110b75445111
myukang <myunghwan0421@gmail.com>
2023-01-14 08:25:18 +0900

    FEAT: route하는 경로의 GlobalPrefix로 api를 붙인다. 리액트를 위한 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import './dotenv'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  //모든 route 경로를 api를 붙인다.
+  app.setGlobalPrefix('api')
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
