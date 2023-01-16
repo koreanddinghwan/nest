52bdc0979e21e8f294e0a21da0169c261f2b05cf
myukang <myunghwan0421@gmail.com>
2023-01-16 09:00:31 +0900

    FEAT: appModule에 typeorm.forRoot로 datasource생성에 필요한 인자정보 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppService } from './app.service'
 import { CatsModule } from './cats/cats.module'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DataSource } from 'typeorm'
+import { User } from './users/users.entity'
 
 @Module({
   imports: [
@@ -11,11 +16,31 @@ import { join } from 'path'
       rootPath: join(__dirname, '..', 'client'),
       exclude: ['/api*'],
     }),
-    CatsModule,
+    /*
+     * TypeOrmModule 전체에 datasource 생성자에 필요한 connection정보 등록
+     * */
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: Number.parseInt(process.env.PORT) | 3306,
+      username: process.env.DBUSER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [User],
+      synchronize: true, //특정 조건하에서 모든 데이터를 삭제하는 것 같습니다. 프로덕션에서는 사용하지 않는게 좋습니다.
+      logging: true,
+    }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  // constructor(private DataSource: DataSource) {}
+  /*
+   * Appmodule이 initialize될때 Datasource를 가집니다.
+   * */
+  constructor(private DataSource: DataSource) {
+    console.log('Datasource initialized')
+  }
 }
