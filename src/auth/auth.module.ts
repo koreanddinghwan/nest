2a52c6e1ececc23138ac60f11699c3434c37355f
myukang <myunghwan0421@gmail.com>
2023-01-22 12:36:42 +0900

    FEAT:auth module add refreshtoken service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,20 +4,22 @@ import { AuthService } from './auth.service'
 import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './auth.controller'
 import { JwtModule } from '@nestjs/jwt'
-import { LocalStrategy } from './local.strategy'
 import { JwtStrategy } from './jwt.stategy'
 import { GoogleStrategy } from './google.strategy'
+import { RefreshTokenService } from './refresh-token/refresh-token.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RefreshToken } from './refresh-token/refreshToken.entity'
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([RefreshToken]),
     UsersModule,
     PassportModule,
     JwtModule.register({
       secret: process.env.JWTCONSTANTS,
-      signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy, GoogleStrategy],
+  providers: [AuthService, JwtStrategy, GoogleStrategy, RefreshTokenService],
   controllers: [AuthController],
   exports: [AuthService],
 })
