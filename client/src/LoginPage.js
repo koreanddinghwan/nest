36cbb7ecd61d07a0cce6e42ae42d17e97ce1b0dc
myukang <myunghwan0421@gmail.com>
2023-01-22 12:37:17 +0900

    FEAT: front test

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,7 +1,38 @@
+import { useLayoutEffect } from 'react'
+
 function LoginPage() {
+  const onGoogleLogin = (e) => {
+    console.log('evemt')
+    window.location.href = 'http://localhost:3000/api/auth/google/login'
+  }
+
+  const refreshSilent = async () => {
+    console.log('post refresh token')
+    //refresh with cookie
+    try {
+      const res = await fetch('http://localhost:3000/api/auth/refresh_silent', {
+        method: 'Get',
+        mode: 'no-cors',
+        //credentials true로 해야 header의 cookie가 전송됩니다.
+        credentials: 'include',
+      })
+      console.log('res:', res.body)
+    } catch (e) {
+      console.log('error')
+    }
+  }
+
+  useLayoutEffect(() => {
+    refreshSilent()
+    //cleanup fnc
+    return () => {
+      console.log('cleanup')
+    }
+  })
+
   return (
     <div>
-      <button>구글 로그인</button>
+      <button onClick={onGoogleLogin}>구글 로그인</button>
     </div>
   )
 }
